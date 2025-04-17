efffec9f9b3156a22c1cfc1506699b64de0c73ff
ShamilShigapov <sshigapov1990@gmail.com>
2025-04-17 21:54:56 +0300

    Добавил ДЗ от 07.04.2025

---
+++ C#_DZ/DZ_07_04_25/AdvertisingPlatforms.cs
@@ -0,0 +1,131 @@
+﻿
+
+namespace C__DZ.DZ_07_04_25;
+
+public class AdvertisingPlatforms
+{
+    private Dictionary<string, List<string>> platformsByLocation;
+
+    public AdvertisingPlatforms()
+    {
+        platformsByLocation = [];
+    }
+
+    public void AddAdvertisingPlatform(string advertisingPlatformAndLocations)
+    {
+        // разделяем строку на платформу и список локаций
+        var elements = advertisingPlatformAndLocations.Split(':');
+
+        // разделяем список локаций на отдельные локации (если их несколько)
+        var locations = elements[1].Split(',');
+
+        // обозначаем текущую платформу
+        var currentPlatform = elements[0];
+
+        // заполняем словарь данными
+        if (!platformsByLocation.ContainsKey(currentPlatform))
+        {
+            platformsByLocation[currentPlatform] = [];
+        }
+
+        for (int i = 0; i < locations.Length; i++)
+        {
+            platformsByLocation[currentPlatform].Add(locations[i]);
+        }
+    }
+
+    // дял проверки введённой локации
+    private static bool CheckInput(string location)
+    {
+        string specialChars = "abcdefghijklmnopqrstuvwxyz/";
+
+        // если пустая строка
+        if (string.IsNullOrEmpty(location))
+        {
+            return false;
+        }
+
+        // первым симолом должен быть /
+        if (location[0] !=  '/' || location.Length == 1)
+        {
+            return false;
+        }
+
+        // если символ / повторяется 2 раза подряд
+        if (location.IndexOf("//") != -1)
+        {
+            return false;
+        }
+
+        // проверяем все ли символы в location состоят только из specialChars
+        var isValid = location.All(c => specialChars.Contains(c));
+
+        return isValid;
+    }
+
+    // для ввода локации с клавиатуры
+    public static string InputLocation()
+    {
+        string inputLocation;
+
+        while (true)
+        {
+            Console.WriteLine("Введите локацию для поиска подходящих рекламных площадок:");
+            inputLocation = Console.ReadLine();
+
+            if (CheckInput(inputLocation) && inputLocation != "")
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод!");
+                Console.WriteLine("Допускаются только латинские буквы в нижнем регистре и символ /");
+                Console.WriteLine("Например: /ru или /ru/msk\n");
+            }
+        }
+
+        return inputLocation;
+    }
+
+    public List<string> GetPlatformsByLocation(string location)
+    {
+        // по мере выполнения программы location будет изменяться
+        // чтобы избежать этого заведем временную переменную
+        string temp = location;
+        // переменная для хранения позиции символа '/'
+        int position;
+        // сюда будем записывать площадки, которые действуют в заданной location
+        var platforms = new List<string>();
+
+        while (temp != "")
+        {
+            foreach (var platform in platformsByLocation)
+            {
+                // если входящая локация совпадает с локацией в текущей платформе
+                if (platform.Value.Contains(temp))
+                {
+                    // добавляем в список
+                    platforms.Add(platform.Key);
+                }
+            }
+
+            //if (platforms.Count == 0)
+            //{
+            //    continue;
+            //}
+            // т.к. локации могут быть вложенными, необходимо постепенно сокращать temp
+            // пример: было - /ru/sverd, стало /ru
+            // для этого:
+            // находим позицию последнего вхождения символа '/'
+            position = temp.LastIndexOf('/');
+            // сокращаем temp
+            temp = temp.Substring(0, position);
+            // далее снова по циклу ищем совпадения по сокращенному temp
+        }
+
+        return platforms;
+    }
+}
+
+
--- C#_DZ/Program.cs
+++ C#_DZ/Program.cs
@@ -1,69 +1,30 @@
-﻿using C__DZ.DZ_09_04_25;
-using CSharp_DZ.DZ_09_04_25;
+﻿using C__DZ.DZ_07_04_25;
 
-//var artwork = new Artwork("", "", "", 0);
-var artwork1 = new Artwork("Мона Лиза", "Леонардо да Винчи", "Высокое Возрождение", 1506);
-var artwork2 = new Artwork("Герника", "Пабло Пикассо", "Кубизм", 1937);
-var artwork3 = new Artwork("Крик", "Эдвард Мунк", "Экспрессионизм", 1893);
-var artwork4 = new Artwork("Постоянство памяти", "Сальвадор Дали", "Сюрреализм", 1931);
-var artwork5 = new Artwork("Музыкант", "Пабло Пикассо", "Кубизм", 1972);
-var artwork6 = new Artwork("Сон", "Сальвадор Дали", "Аллегорическая сцена", 1937);
+var advertisingPlatforms = new AdvertisingPlatforms();
 
-var gallery = new ArtCollection();
+advertisingPlatforms.AddAdvertisingPlatform("Яндекс.Директ:/ru");
+advertisingPlatforms.AddAdvertisingPlatform("Ревдинский рабочий:/ru/svrd/revda,/ru/svrd/pervik");
+advertisingPlatforms.AddAdvertisingPlatform("Газета уральских москвичей:/ru/msk,/ru/permobl,/ru/chelobl");
+advertisingPlatforms.AddAdvertisingPlatform("Крутая реклама:/ru/svrd");
 
-gallery.AddArtwork(artwork1);
-gallery.AddArtwork(artwork2);
-gallery.AddArtwork(artwork3);
-gallery.AddArtwork(artwork3);
-gallery.AddArtwork(artwork4);
-gallery.AddArtwork(artwork5);
-gallery.AddArtwork(artwork6);
+var platforms = new List<string>();
 
-Console.WriteLine(gallery);
+var location = "/ru/msk";
 
-gallery.RemoveArtwork("Крик", 1893);
+//var location = AdvertisingPlatforms.InputLocation();
 
-Console.WriteLine(gallery);
+platforms = advertisingPlatforms.GetPlatformsByLocation(location);
 
-var artworksByArtistList = gallery.FindArtworksByArtist("Пабло Пикассо");
-
-Console.WriteLine("Произведения Пабло Пикассо:");
-foreach (var artwork in artworksByArtistList)
+if (platforms.Count == 0)
 {
-    Console.WriteLine(artwork);
+    Console.WriteLine($"Рекламных платформ, действующих в локации {location} не найдено");
 }
-
-var artworksByStyle = gallery.GetArtworksByStyle("Кубизм");
-
-Console.WriteLine("Произведения в стиле кубизм:");
-foreach (var artwork in artworksByStyle)
+else
 {
-    Console.WriteLine(artwork);
-}
+    Console.WriteLine($"Список рекламных платформ, действующих в локации: {location}");
 
-var artworksGroupedByYear = gallery.GroupByYear(1937);
-
-Console.WriteLine("Произведения, написанные в 1937:");
-foreach (var artwork in artworksGroupedByYear)
-{
-    Console.WriteLine(artwork);
-}
-
-Dictionary<string, List<Artwork>> artworksByArtist = new Dictionary<string, List<Artwork>>();
-
-for (int i = 0; i < gallery.Count; i++)
-{
-    if (!artworksByArtist.ContainsKey(gallery[i].Artist))
+    foreach (var platform in platforms)
     {
-        artworksByArtist[gallery[i].Artist] = [];
+        Console.WriteLine(platform);
     }
-
-    artworksByArtist[gallery[i].Artist].Add(gallery[i]);
-}
-
-HashSet<string> artworksTitles = new HashSet<string>();
-
-for (int i = 0; i < gallery.Count; i++)
-{
-    artworksTitles.Add(gallery[i].Title);
 }
