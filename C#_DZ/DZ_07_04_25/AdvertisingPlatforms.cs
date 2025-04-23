41b6aff42773e26491aa0fa2292d565e77a2766c
ShamilShigapov <sshigapov1990@gmail.com>
2025-04-23 13:21:03 +0300

    Добавил исправленное ДЗ от 07.04.2025

--- C#_DZ/DZ_07_04_25/AdvertisingPlatforms.cs
+++ C#_DZ/DZ_07_04_25/AdvertisingPlatforms.cs
@@ -1,5 +1,8 @@
 ﻿
 
+using System;
+using System.Linq;
+
 namespace C__DZ.DZ_07_04_25;
 
 public class AdvertisingPlatforms
@@ -11,30 +14,133 @@ public class AdvertisingPlatforms
         platformsByLocation = [];
     }
 
-    public void AddAdvertisingPlatform(string advertisingPlatformAndLocations)
+    // обновление по вложенности
+    // проверяем по каждому ключу могут ли быть вложены в него другие ключи
+    private void UpdatingByNesting2()
     {
-        // разделяем строку на платформу и список локаций
-        var elements = advertisingPlatformAndLocations.Split(':');
-
-        // разделяем список локаций на отдельные локации (если их несколько)
-        var locations = elements[1].Split(',');
-
-        // обозначаем текущую платформу
-        var currentPlatform = elements[0];
-
-        // заполняем словарь данными
-        if (!platformsByLocation.ContainsKey(currentPlatform))
+        foreach (var location in platformsByLocation)
         {
-            platformsByLocation[currentPlatform] = [];
+            var currentLocation = location.Key;
+            int position;
+
+            position = currentLocation.LastIndexOf("/");
+            currentLocation = currentLocation[..position];
+
+            while (currentLocation != "")
+            {
+                foreach (var location2 in platformsByLocation)
+                {
+                    if (currentLocation == location2.Key && location.Key.GetHashCode() != location2.Key.GetHashCode())
+                    {
+                        location.Value.AddRange(location2.Value);
+                    }
+                }
+
+                position = currentLocation.LastIndexOf("/");
+                currentLocation = currentLocation[..position];
+            }
         }
 
-        for (int i = 0; i < locations.Length; i++)
+        // т.к. значения по ключам могут повторяться, используем Distinct
+        foreach (var location in platformsByLocation)
         {
-            platformsByLocation[currentPlatform].Add(locations[i]);
+            platformsByLocation[location.Key] = [.. location.Value.Distinct()]; // location.Value.Distinct().ToList()
         }
     }
 
-    // дял проверки введённой локации
+    // обновление по вложенности
+    // после добавления новой локации, проверяем 
+    // сначала может ли быть она быть вложена в другие существующие локации
+    // затем могут ли в неё быть добавлены существующие локации
+    private void UpdatingByNesting(string[] locations, string advertisingPlatform)
+    {
+        // проверяем вложенность локаций
+        for (int i = 0; i < locations.Length; i++)
+        {
+            // по мере выполнения программы location[i] будет изменяться
+            // чтобы избежать этого заведем временную переменную
+            var tempLocation = locations[i];
+            // переменная для хранения позиции символа '/'
+            int position;
+
+            // т.к. локации могут быть вложенными, необходимо постепенно сокращать tempLocation
+            // пример: было - /ru/sverd, стало /ru
+            // для этого:
+            // находим позицию последнего вхождения символа '/'
+            position = tempLocation.LastIndexOf('/');
+            // сокращаем temp
+            tempLocation = tempLocation[..position]; //temp.Substring(0, position)
+            // далее по циклу ищем совпадения по сокращенному temp
+
+            // проверяем может ли текущая локация быть вложенной в другую, уже существующую
+            // если да, в текущую локацию долбавляем площадки, в локации которых она может быть вложена
+            while (tempLocation != "")
+            {
+                foreach (var location in platformsByLocation)
+                {
+                    // если tempLocation совпадает с локацией в текущей площадке
+                    // (если текущая локация locations[i] вложена в сокращённую локацию tempLocation)
+                    if (location.Key == tempLocation)
+                    {
+                        // добавляем в список текущей локации locations[i] список площадок из сокращёной локации tempLocation
+                        platformsByLocation[locations[i]].AddRange(location.Value);
+                    }
+                }
+
+                position = tempLocation.LastIndexOf('/');
+
+                tempLocation = tempLocation[..position];
+            }
+
+            // проверяем могут ли в текущую локацию быть вложены другие, уже существующие
+            // если да, в уже существующую локацию добавляем площадку текущей локации
+            foreach (var location in platformsByLocation)
+            {
+                var tempLocation2 = location.Key;
+                int position2;
+
+                position2 = tempLocation2.LastIndexOf("/");
+                tempLocation2 = tempLocation2[..position2];
+
+                while (tempLocation2 != "")
+                {
+
+                    if (tempLocation2 == locations[i])
+                    {
+                        location.Value.Add(advertisingPlatform);
+                    }
+
+                    position2 = tempLocation2.LastIndexOf("/");
+                    tempLocation2 = tempLocation2[..position2];
+                }
+            }
+        }
+    }
+
+    public void AddAdvertisingPlatform(string advertisingPlatformAndLocations)
+    {
+        var elements = advertisingPlatformAndLocations.Split(':');
+
+        var locations = elements[1].Split(',');
+
+        for (int i = 0; i < locations.Length; i++)
+        {
+            if (!platformsByLocation.ContainsKey(locations[i]))
+            {
+                platformsByLocation[locations[i]] = [];
+            }
+
+            platformsByLocation[locations[i]].Add(elements[0]);
+        }
+
+        UpdatingByNesting(locations, elements[0]);
+        //UpdatingByNesting2();
+    }
+
+    public List<string> GetPlatformsByLocation(string location)
+    {
+        return platformsByLocation[location];
+    }
     private static bool CheckInput(string location)
     {
         string specialChars = "abcdefghijklmnopqrstuvwxyz/";
@@ -73,7 +179,7 @@ public class AdvertisingPlatforms
             Console.WriteLine("Введите локацию для поиска подходящих рекламных площадок:");
             inputLocation = Console.ReadLine();
 
-            if (CheckInput(inputLocation) && inputLocation != "")
+            if (CheckInput(inputLocation))
             {
                 break;
             }
@@ -88,44 +194,65 @@ public class AdvertisingPlatforms
         return inputLocation;
     }
 
-    public List<string> GetPlatformsByLocation(string location)
-    {
-        // по мере выполнения программы location будет изменяться
-        // чтобы избежать этого заведем временную переменную
-        string temp = location;
-        // переменная для хранения позиции символа '/'
-        int position;
-        // сюда будем записывать площадки, которые действуют в заданной location
-        var platforms = new List<string>();
+    //public List<string> GetPlatformsByLocation(string location)
+    //{
+    //    // по мере выполнения программы location будет изменяться
+    //    // чтобы избежать этого заведем временную переменную
+    //    string temp = location;
+    //    // переменная для хранения позиции символа '/'
+    //    int position;
+    //    // сюда будем записывать площадки, которые действуют в заданной location
+    //    var platforms = new List<string>();
 
-        while (temp != "")
-        {
-            foreach (var platform in platformsByLocation)
-            {
-                // если входящая локация совпадает с локацией в текущей платформе
-                if (platform.Value.Contains(temp))
-                {
-                    // добавляем в список
-                    platforms.Add(platform.Key);
-                }
-            }
+    //    while (temp != "")
+    //    {
+    //        foreach (var platform in platformsByLocation)
+    //        {
+    //            // если входящая локация совпадает с локацией в текущей платформе
+    //            if (platform.Value.Contains(temp))
+    //            {
+    //                // добавляем в список
+    //                platforms.Add(platform.Key);
+    //            }
+    //        }
 
-            //if (platforms.Count == 0)
-            //{
-            //    continue;
-            //}
-            // т.к. локации могут быть вложенными, необходимо постепенно сокращать temp
-            // пример: было - /ru/sverd, стало /ru
-            // для этого:
-            // находим позицию последнего вхождения символа '/'
-            position = temp.LastIndexOf('/');
-            // сокращаем temp
-            temp = temp.Substring(0, position);
-            // далее снова по циклу ищем совпадения по сокращенному temp
-        }
+    //        // т.к. локации могут быть вложенными, необходимо постепенно сокращать temp
+    //        // пример: было - /ru/sverd, стало /ru
+    //        // для этого:
+    //        // находим позицию последнего вхождения символа '/'
+    //        position = temp.LastIndexOf('/');
+    //        // сокращаем temp
+    //        temp = temp[..position]; //temp.Substring(0, position)
+    //        // далее снова по циклу ищем совпадения по сокращенному temp
+    //    }
 
-        return platforms;
-    }
+    //    return platforms;
+    //}
+
+    // дял проверки введённой локации
+
+    //public void AddAdvertisingPlatform(string advertisingPlatformAndLocations)
+    //{
+    //    // разделяем строку на платформу и список локаций
+    //    var elements = advertisingPlatformAndLocations.Split(':');
+
+    //    // разделяем список локаций на отдельные локации (если их несколько)
+    //    var locations = elements[1].Split(',');
+
+    //    // обозначаем текущую платформу
+    //    var currentPlatform = elements[0];
+
+    //    // заполняем словарь данными
+    //    if (!platformsByLocation.ContainsKey(currentPlatform))
+    //    {
+    //        platformsByLocation[currentPlatform] = [];
+    //    }
+
+    //    for (int i = 0; i < locations.Length; i++)
+    //    {
+    //        platformsByLocation[currentPlatform].Add(locations[i]);
+    //    }
+    //}
 }
 
 
--- C#_DZ/Program.cs
+++ C#_DZ/Program.cs
@@ -2,29 +2,22 @@
 
 var advertisingPlatforms = new AdvertisingPlatforms();
 
-advertisingPlatforms.AddAdvertisingPlatform("Яндекс.Директ:/ru");
 advertisingPlatforms.AddAdvertisingPlatform("Ревдинский рабочий:/ru/svrd/revda,/ru/svrd/pervik");
 advertisingPlatforms.AddAdvertisingPlatform("Газета уральских москвичей:/ru/msk,/ru/permobl,/ru/chelobl");
 advertisingPlatforms.AddAdvertisingPlatform("Крутая реклама:/ru/svrd");
+advertisingPlatforms.AddAdvertisingPlatform("Яндекс.Директ:/ru");
 
 var platforms = new List<string>();
 
-var location = "/ru/msk";
+var location = "/ru/chelobl";
 
 //var location = AdvertisingPlatforms.InputLocation();
 
 platforms = advertisingPlatforms.GetPlatformsByLocation(location);
 
-if (platforms.Count == 0)
-{
-    Console.WriteLine($"Рекламных платформ, действующих в локации {location} не найдено");
-}
-else
-{
-    Console.WriteLine($"Список рекламных платформ, действующих в локации: {location}");
+Console.WriteLine($"Список рекламных платформ, действующих в локации: {location}");
 
-    foreach (var platform in platforms)
-    {
-        Console.WriteLine(platform);
-    }
+foreach (var platform in platforms)
+{
+    Console.WriteLine(platform);
 }
