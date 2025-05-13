8b9c47a12d8b657f40e85eab157948ad5d1b39f9
ShamilShigapov <sshigapov1990@gmail.com>
2025-05-13 19:30:52 +0300

    ДЗ от 30.04.2025

---
+++ C#_DZ/DZ_30_04_25/MyLinkedList.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+
+namespace C__DZ.DZ_30_04_25;
+
+public class MyLinkedList<T> : IEnumerable<T>
+{
+    private class Node<T>
+    {
+        public T? Value { get; set; }
+
+        public Node<T>? Next { get; set; }
+    }
+
+    private Node<T>? _root;
+
+    public int Count { get; private set; }
+
+    public void Add(T data)
+    {
+        var node = new Node<T> { Value = data };
+
+        if (_root == null)
+        {
+            _root = node;
+        }
+        else
+        {
+            var currentRoot = _root;
+
+            while (currentRoot.Next != null)
+            {
+                currentRoot = currentRoot.Next;
+            }
+            
+            currentRoot.Next = node;
+        }
+
+        Count++;
+    }
+
+    public bool Remove(T data)
+    {
+        var currentNode = _root;
+        Node<T>? previosNode = null;
+
+        while (currentNode != null)
+        {
+            if (currentNode.Value.Equals(data))
+            {
+                if (previosNode == null)
+                {
+                    _root = _root.Next;
+                }
+                else
+                {
+                    previosNode.Next = currentNode.Next;
+                }
+
+                Count--;
+                return true;
+            }
+
+            previosNode = currentNode;
+            currentNode = currentNode.Next;
+        }
+
+        return false;
+    }
+
+    public bool Contains(T data)
+    {
+        var currentRoot = _root;
+
+        while (currentRoot != null)
+        {
+            if (currentRoot.Value.Equals(data))
+            {
+                return true;
+            }
+
+            currentRoot = currentRoot.Next;
+        }
+
+        return false;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        var currentRoot = _root;
+
+        while (currentRoot != null)
+        {
+            yield return currentRoot.Value;
+            currentRoot = currentRoot.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
--- C#_DZ/Program.cs
+++ C#_DZ/Program.cs
@@ -1,145 +1,37 @@
-﻿using C__DZ.DZ_18_04_25.Task1;
-using C__DZ.DZ_18_04_25.Task2;
-using C__DZ.DZ_18_04_25.Task3;
-using C__DZ.DZ_18_04_25.Task4;
-using C__DZ.DZ_18_04_25.Task5;
+﻿using C__DZ.DZ_30_04_25;
 
 
-////// Задача 1 -------------------------------------------------------
+var myLinkedList = new MyLinkedList<string>();
 
-var students = new List<C__DZ.DZ_18_04_25.Task1.Student>
+Console.WriteLine($"{myLinkedList.Contains("Катя")}");
+Console.WriteLine();
+
+myLinkedList.Add("Вася");
+myLinkedList.Add("Петя");
+myLinkedList.Add("Оля");
+myLinkedList.Add("Маша");
+myLinkedList.Add("Катя");
+
+Console.WriteLine($"{myLinkedList.Count}");
+foreach (var item in myLinkedList)
 {
-     new C__DZ.DZ_18_04_25.Task1.Student { Name = "Alice", Score = 90 },
-     new C__DZ.DZ_18_04_25.Task1.Student { Name = "Bob", Score = 80 },
-     new C__DZ.DZ_18_04_25.Task1.Student { Name = "Charlie", Score = 88 },
-     new C__DZ.DZ_18_04_25.Task1.Student { Name = "David", Score = 92 }
-};
-
-var sortedStudents = students.Where(student => student.Score > 85).ToList();
-
-Console.WriteLine("Студенты с оценкой выше 85:");
-foreach (var student in sortedStudents)
-{
-    Console.WriteLine(student);
+    Console.WriteLine(item);
 }
+Console.WriteLine();
 
+Console.WriteLine($"{myLinkedList.Contains("Катя")}");
+Console.WriteLine();
 
-////// Задача 2 -------------------------------------------------------
+Console.WriteLine($"{myLinkedList.Contains("Саша")}");
+Console.WriteLine();
 
-var books = new List<Book>
+myLinkedList.Remove("Вася");
+myLinkedList.Remove("Оля");
+myLinkedList.Remove("Катя");
+
+Console.WriteLine($"{myLinkedList.Count}");
+foreach (var item in myLinkedList)
 {
-     new Book { Title = "1984" },
-     new Book { Title = "To Kill a Mockingbird" },
-     new Book { Title = "The Great Gatsby" }
-};
-
-var orderedBooks = from b in books
-                    orderby b.Title
-                    select b;
-
-//var orderedBooks = books.OrderBy(b => b.Title).ToList();
-
-Console.WriteLine("\nКниги после сортировки:");
-
-foreach (var book in orderedBooks)
-{
-    Console.WriteLine($"Название: {book.Title}");
+    Console.WriteLine(item);
 }
-
-
-////// Задача 3 -------------------------------------------------------
-
-var products = new List<Product>
-{
-     new Product { Name = "Apple", Category = "Fruits" },
-     new Product { Name = "Carrot", Category = "Vegetables" },
-     new Product { Name = "Banana", Category = "Fruits" },
-     new Product { Name = "Broccoli", Category = "Vegetables" }
-};
-
-var groupedProducts = from product in products
-                      group product by product.Category into c
-                      select new
-                      {
-                          Category = c.Key,
-                          Products = from p in c select p
-                      };
-
-
-//var groupedProducts = products
-//                            .GroupBy(p => p.Category)
-//                            .Select(c => new 
-//                            {
-//                            Category = c.Key,
-//                            Products = c.Select(p => p)
-//                            });
-
-Console.WriteLine("\nПродукты по категориям:");
-foreach (var category in groupedProducts)
-{
-    Console.WriteLine($"Категория: {category.Category}");
-
-    foreach (var product in category.Products)
-    {
-        Console.WriteLine($"Продукт: {product.Name}");
-    }
-}
-
-
-////// Задача 4 -------------------------------------------------------
-
-var students2 = new List<C__DZ.DZ_18_04_25.Task4.Student>
-{
-     new C__DZ.DZ_18_04_25.Task4.Student { Id = 1, Name = "Alice" },
-     new C__DZ.DZ_18_04_25.Task4.Student { Id = 2, Name = "Bob" }
-};
-var grades = new List<Grade>
-{
-     new Grade { StudentId = 1, Subject = "Math", LetterGrade = 'A' },
-     new Grade { StudentId = 2, Subject = "Math", LetterGrade = 'B' },
-     new Grade { StudentId = 1, Subject = "Science", LetterGrade = 'A' }
-};
-
-//var studentsProgress = students2.Join(grades,
-//                                        s => s.Id,
-//                                        g => g.StudentId,
-//                                        (s, g) => new
-//                                        {
-//                                            StudentId = s.Id,
-//                                            Name = s.Name,
-//                                            Subject = g.Subject,
-//                                            LetterGrade = g.LetterGrade
-//                                        });
-
-var studentsProgress = from s in students2
-                       join g in grades on s.Id equals g.StudentId
-                       select new
-                       {
-                           StudentId = s.Id,
-                           Name = s.Name,
-                           Subject = g.Subject,
-                           LetterGrade = g.LetterGrade
-                       };
-
-
-Console.WriteLine("\nОбъединённые классы Student и Grade:");
-foreach (var student in studentsProgress)
-{
-    Console.WriteLine($"ID: {student.StudentId}, Имя: {student.Name}");
-    Console.WriteLine($"Предмет: {student.Subject}, Оценка: {student.LetterGrade}");
-}
-
-
-////// Задача 5 -------------------------------------------------------
-
-var orders = new List<Order>
-{
-     new Order { Amount = 150.00m },
-     new Order { Amount = 200.00m },
-     new Order { Amount = 75.00m }
-};
-
-var ordersAmount = orders.Sum(o => o.Amount);
-
-Console.WriteLine("\nОбщая сумма заказов:");
-Console.WriteLine(ordersAmount);
+Console.WriteLine();
